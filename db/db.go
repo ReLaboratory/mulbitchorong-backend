1151f6809339780ec1043057f7cc4079944a6af4
Yunseo Hwang <yunseo.h68@gmail.com>
2019-11-14 15:50:10 +0900

    Feat : Upload API 핸들러 추가

--- db/db.go
+++ db/db.go
@@ -5,7 +5,6 @@ import "gopkg.in/mgo.v2"
 // MongoDB 구조체는 몽고DB에 대한 정보를 담는 구조체입니다.
 type MongoDB struct {
 	Session *mgo.Session
-	DB      *mgo.Database
 }
 
 // NewMongoDB 메서드는 MongoDB 객체와 MongoDB Session을 생성합니다.
@@ -17,8 +16,6 @@ func NewMongoDB(addr string) (*MongoDB, error) {
 	}
 
 	m := &MongoDB{}
-	m.DB = new(mgo.Database)
-	m.DB.Session = s
 	m.Session = s
 	return m, nil
 }
--- handle/upload.go
+++ handle/upload.go
@@ -6,29 +6,47 @@ import (
 	"io"
 	"mime/multipart"
 	"net/http"
+	"path/filepath"
 
 	"github.com/julienschmidt/httprouter"
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
+
+// FileMeta 는 Upload할 파일의 메타정보를 담는 구조체입니다.
+type FileMeta struct {
+	Inode int
+	Uname string
+}
+
+type res struct {
+	IsSuccess bool `json:"isSuccess"`
+}
 
 // Upload 함수는 유저 정보와 사용자 정보를 받아 이미지 업로드 기능을 수행하는 핸들러입니다.
 func Upload(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
-	for _, fileHeaders := range req.MultipartForm.File {
-		for _, fileHeader := range fileHeaders {
-			file, _ := fileHeader.Open()
-			gridFile, err := mongoDB.DB.GridFS("fs").Create(fileHeader.Filename)
-			if err != nil {
-				//errorResponse(w, err, http.StatusInternalServerError)
-				return
-			}
-			// gridFile.SetMeta()
-			gridFile.SetName(fileHeader.Filename)
-			if err := writeToGridFile(file, gridFile); err != nil {
-				//errorResponse(w, err, http.StatusInternalServerError)
-				return
-			}
-		}
+	uploadRes := new(res)
+
+	req.ParseForm()
+	_, fh, err := req.FormFile("files")
+	iname := req.FormValue("imgName")
+	uname := req.FormValue("uname")
+
+	file, _ := fh.Open()
+	uFileName := string(iname + filepath.Ext(fh.Filename))
+	gridFile, err := mongoDB.Session.DB("test").GridFS("fs").Create(uFileName)
+	if err != nil {
+		uploadRes.IsSuccess = false
 	}
+	gridFile.SetMeta(bson.M{"user": uname})
+	gridFile.SetName(uFileName)
+
+	if err := writeToGridFile(file, gridFile); err != nil {
+		uploadRes.IsSuccess = false
+	} else {
+		uploadRes.IsSuccess = true
+	}
+	renderer.JSON(w, http.StatusCreated, uploadRes)
 }
 
 func writeToGridFile(file multipart.File, gridFile *mgo.GridFile) error {
