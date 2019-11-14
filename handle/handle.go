4711fc7eae11a8ae6f43441011da71a205af18e0
Yunseo Hwang <yunseo.h68@gmail.com>
2019-11-14 19:53:30 +0900

    Fix : handle 패키지의 존재하는 구조체들을 handle.go로 옮김

--- handle/handle.go
+++ handle/handle.go
@@ -6,6 +6,29 @@ import (
 	"github.com/unrolled/render"
 )
 
+// ImageFile 은 이미지 파일에 대한 정보를 담고 있는 구조체입니다.
+type ImageFile struct {
+	Filename string   `bson:"filename"`
+	MetaData FileMeta `bson:"metadata"`
+}
+
+// ImageName 은 이미지 파일의 이름을 담고 있는 구조체입니다.
+type ImageName struct {
+	Name string `json:"imgName"`
+}
+
+// FileMeta 는 Upload할 파일의 메타정보를 담는 구조체입니다.
+type FileMeta struct {
+	Inode int
+	UID   string `bson:"uid" json:"uid"`
+	Ext   string `bson:"ext" json:"ext"`
+}
+
+// Res 는 Default 응답값인 성공여부를 담는 구조체입니다.
+type Res struct {
+	IsSuccess bool `json:"isSuccess"`
+}
+
 var (
 	renderer *render.Render
 	mongoDB  *db.MongoDB
