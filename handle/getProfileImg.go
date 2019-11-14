12a9701f5cfd0030579d92a158e02395068c7ba6
Yunseo Hwang <yunseo.h68@gmail.com>
2019-11-14 23:19:58 +0900

    Feat : 아이디에 따른 프로필 이미지를 응답하는 API 추가

--- handle/getProfileImg.go
+++ handle/getProfileImg.go
@@ -1,10 +1,37 @@
 package handle
 
 import (
+	"log"
+	"mulbitchorong-backend/user"
 	"net/http"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2/bson"
 )
 
 // GetProfileImg 함수는 유저의 ID값에 맞는 데이터를 조회하여 해당하는 유저의 프로필 이미지를 응답하는 핸들러입니다.
-func GetProfileImg(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {}
+func GetProfileImg(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+	uid := ps.ByName("id")
+
+	session := mongoDB.Session.Copy()
+	defer session.Close()
+
+	c := session.DB("test").C("users")
+	u := user.New()
+	err := c.Find(bson.M{"uid": uid}).One(&u)
+
+	name := u.ProfileImg
+
+	gridfs := mongoDB.Session.DB("test").GridFS("fs")
+
+	f, err := gridfs.Open(name)
+	if err != nil {
+		log.Printf("Failed to open %s: %v", name, err)
+		http.Error(w, "something went wrong", http.StatusInternalServerError)
+		return
+	}
+	defer f.Close()
+
+	http.ServeContent(w, req, name, time.Now(), f)
+}
